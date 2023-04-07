ab921edde145c4429fca7ac5dfc77e4894064734
Robin Son <huuson1401@gmail.com>
2023-04-07 21:54:22 +0700

    styles layout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'
+
+
+const cx = classNames.bind(styles)
+
 function Header() {
-    return <h2>Header</h2>
+    return <Header classNames={cx('wrapper')}>
+        <div className={cx('inner')}>
+
+        </div>
+    </Header>
 }
 
 export default Header;
